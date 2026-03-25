6f39e38c14a91703c1aa103939210e9caf337267
nayeem-94 <nayeem11720@gmail.com>
2026-03-25 13:59:58 +0600

    addded the navber using login

--- src/components/Header/Navber.jsx
+++ src/components/Header/Navber.jsx
@@ -1,10 +1,72 @@
-import React from 'react';
-import { NavLink } from 'react-router';
+import React, { useContext, useState } from 'react';
+import { Navigate, NavLink, useNavigate } from 'react-router';
+import { AuthContext } from '../../pages/Provider/Authprovider';
+import Swal from 'sweetalert2';
 
 const Navber = () => {
+
+    const { user, logOut } = useContext(AuthContext);
+    const [loggingOut, setLoggingOut] = useState(false);
+
+    const isLoggedIn = !!user;
+    const Navigate = useNavigate();
+
+
+
+    const handleLogout = () => {
+
+        setLoggingOut(true);
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You will be logged out!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#f59e0b', // amber
+            cancelButtonColor: '#d1d5db', // gray
+            confirmButtonText: 'Yes, log me out!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                logOut()
+                    .then(() => {
+                        Swal.fire({
+                            title: 'Logged out!',
+                            text: 'You have successfully logged out.',
+                            icon: 'success',
+                            timer: 1500,
+                            showConfirmButton: false
+                        }).then(() => {
+
+                            setTimeout(() => {
+                                setLoggingOut(false); // hide spinner
+                                navigate("/home"); // redirect to home after logout
+                            }, 1000);
+                            
+                        });
+                    })
+                    .catch((error) => {
+                        setLoggingOut(false);
+                        Swal.fire({
+                            title: 'Error!',
+                            text: error.message,
+                            icon: 'error'
+                        });
+                    });
+            }
+        });
+    };
+
+    if (loggingOut) {
+        return (
+            <div className="min-h-screen flex justify-center items-center">
+                <span className="loading loading-bars loading-xl"></span>
+            </div>
+        );
+    }
+
     return (
         <div className="bg-yellow-300 py-3 sm:py-4">
-             {/* <div className="bg-yellow-200/40 py-3 sm:py-4 backdrop-blur-sm"> */}
+            {/* <div className="bg-yellow-200/40 py-3 sm:py-4 backdrop-blur-sm"> */}
             <div className="max-w-6xl mx-auto flex items-center justify-between px-2 sm:px-0">
 
                 {/* Main Rounded Container */}
@@ -42,39 +104,62 @@ const Navber = () => {
                     {/* Right: Buttons */}
                     <div className="flex items-center gap-2">
 
-                        <div className="sm:flex items-center gap-2">
-                            <NavLink to="/login">
-                                <button className="cursor-pointer px-4 py-2 rounded-full bg-black text-white text-sm font-semibold hover:bg-gray-800 transition">
-                                    Login
-                                </button>
-                            </NavLink>
+                        {
+                            isLoggedIn ? (
+                                <>
+                                    <button onClick={handleLogout} className="cursor-pointer bg-amber-400 rounded-full text-black px-4 py-2 text-sm font-medium hover:bg-yellow-300 transition">
+                                        Logout
+                                    </button>
+                                    {/* Profile  */}
+                                    <div className="dropdown dropdown-end">
+                                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                            <div className="w-8 sm:w-10 rounded-full ring ring-gray-200">
+                                                <img
+                                                    alt="profile"
+                                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                            </div>
+                                        </div>
+                                        <ul
+                                            tabIndex={-1}
+                                            className="menu menu-sm dropdown-content bg-white rounded-2xl z-50 mt-3 w-52 p-2 shadow-lg">
+                                            <li><a>👤 Profile</a></li>
+                                            <li><a>➕ Add Food</a></li>
+                                            <li><a>📦 Manage My Foods</a></li>
+                                            <li><a>📩 Requests</a></li>
+                                            <li><a>🚪 Logout</a></li>
+                                        </ul>
+                                    </div>
+                                </>
+                            )
+                                :
+                                (
+                                    <>
+                                        <div className="sm:flex items-center gap-2">
+                                            <NavLink to="/login">
+                                                <button className="cursor-pointer px-4 py-2 rounded-full bg-black text-white text-sm font-semibold hover:bg-gray-800 transition">
+                                                    Login
+                                                </button>
+                                            </NavLink>
 
-                            <NavLink to="/register">
-                                <button className="cursor-pointer bg-amber-400 rounded-full text-black px-4 py-2 text-sm font-medium hover:bg-yellow-300 transition">
-                                    Sign Up
-                                </button>
-                            </NavLink>
-                        </div>
+                                            <NavLink to="/register">
+                                                <button className="cursor-pointer bg-amber-400 rounded-full text-black px-4 py-2 text-sm font-medium hover:bg-yellow-300 transition">
+                                                    Sign Up
+                                                </button>
+                                            </NavLink>
+                                        </div>
+                                    </>
+                                )
 
-                        {/* Profile always visible */}
-                        <div className="dropdown dropdown-end">
-                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                                <div className="w-8 sm:w-10 rounded-full ring ring-gray-200">
-                                    <img
-                                        alt="profile"
-                                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                                </div>
-                            </div>
-                            <ul
-                                tabIndex={-1}
-                                className="menu menu-sm dropdown-content bg-white rounded-2xl z-50 mt-3 w-52 p-2 shadow-lg">
-                                <li><a>👤 Profile</a></li>
-                                <li><a>➕ Add Food</a></li>
-                                <li><a>📦 Manage My Foods</a></li>
-                                <li><a>📩 Requests</a></li>
-                                <li><a>🚪 Logout</a></li>
-                            </ul>
-                        </div>
+
+                        }
+
+
+
+
+
+
+
+
                     </div>
                 </div>
 
